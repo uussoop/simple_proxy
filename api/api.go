83bf7452aa37ef5585d5c7cacd6c515019e72fec
parsa <parsa@beastbrain.org>
2023-07-08 13:26:51 +0330

    fixed streaming

--- api/api.go
+++ api/api.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"path"
+	"strconv"
 	"strings"
 	"time"
 
@@ -13,25 +15,39 @@ import (
 	"github.com/uussoop/simple_proxy/utils"
 )
 
+type streamRequest struct {
+	Stream bool `json:"stream"`
+	// Add other fields of the request body if applicable
+}
+
 var api_key string = utils.Getenv("OPENAI_API_KEY", "")
 
 var domain string = utils.Getenv("OPENAI_DOMAIN", "api.openai.com")
 
 func Forwarder(w http.ResponseWriter, r *http.Request) {
+	var streamBody streamRequest
+	bodyCopy, readErr := io.ReadAll(r.Body)          // Create a copy of the request body
+	r.Body = io.NopCloser(bytes.NewBuffer(bodyCopy)) // Restore the request body with the copy
+
+	streamErr := json.Unmarshal(bodyCopy, &streamBody) // Decode the copied body
+	if streamErr != nil {
+		// Handle JSON decoding error
+		fmt.Printf("Failed to decode JSON: %s\n", streamErr)
+		return
+	}
+
 	ctx := r.Context()
 	fmt.Printf("context: %s\n", ctx.Value("config").(*config.Config).APIKeys)
-	body, error := io.ReadAll(r.Body)
-
-	fmt.Printf(string(body) + "\n")
-	if error != nil {
-		fmt.Printf("error reading body: %s\n", error)
+	fmt.Printf("Body: %s\n", string(bodyCopy))
+	if readErr != nil {
+		fmt.Printf("error reading body: %s\n", readErr)
 	}
 	path := path.Clean(r.URL.Path)
 	// use differ
 
 	defer r.Body.Close()
 
-	req, err := http.NewRequest(strings.ToUpper(r.Method), "https://"+domain+path, bytes.NewBuffer(body))
+	req, err := http.NewRequest(strings.ToUpper(r.Method), "https://"+domain+path, bytes.NewBuffer(bodyCopy))
 	for k, v := range r.Header {
 		if k == "Authorization" {
 			for client, ky := range ctx.Value("config").(*config.Config).APIKeys {
@@ -58,16 +74,45 @@ func Forwarder(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// resp_body := utils.Deflate_gzip(resp)
-	resp_body, err := io.ReadAll(resp.Body)
 
-	fmt.Printf("response body: %s\n", (resp_body))
+	// Check if the value of the "stream" field is true
+	if streamBody.Stream {
 
-	for k, v := range resp.Header {
+		// read resp body chunk chunk until EOF and io write each chunk if available
+		for { // read chunk
+			fmt.Printf("chunk length: %s\n", strconv.FormatInt(resp.ContentLength, 10))
+			buf := make([]byte, 4*1024)
+			n, err := resp.Body.Read(buf)
+			fmt.Printf(string(buf[:n]))
+			if n == 0 {
+				break
+			}
+			if err != nil && err != io.EOF {
+				fmt.Printf("error reading response body: %s\n", err)
+				break
+			}
+			for k, v := range resp.Header {
 
-		w.Header().Add(k, v[0])
-		fmt.Printf("%s: %s\n", k, v[0])
+				w.Header().Add(k, v[0])
+				fmt.Printf("%s: %s\n", k, v[0])
 
+			}
+			w.Write(buf[:n])
+		}
+	} else {
+		resp_body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("error reading response body: %s\n", err)
+		}
+		fmt.Printf("response body: %s\n", (resp_body))
+
+		for k, v := range resp.Header {
+
+			w.Header().Add(k, v[0])
+			fmt.Printf("%s: %s\n", k, v[0])
+
+		}
+		w.WriteHeader(resp.StatusCode)
+		io.WriteString(w, string(resp_body))
 	}
-	w.WriteHeader(resp.StatusCode)
-	io.WriteString(w, string(resp_body))
 }
