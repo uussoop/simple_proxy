aa01c5fb3d4d83e92b9c1f3f056cd0d4ac4bfcfe
parsa <parsa@beastbrain.org>
2023-07-15 14:47:39 +0330

    bug fixs

--- api/api.go
+++ api/api.go
@@ -42,6 +42,8 @@ func Forwarder(w http.ResponseWriter, r *http.Request) {
 		if streamErr != nil {
 			// Handle JSON decoding error
 			fmt.Printf("Failed to decode JSON: %s\n", streamErr)
+			NormalResponse(w, r)
+			return
 
 		}
 
@@ -79,24 +81,27 @@ func Forwarder(w http.ResponseWriter, r *http.Request) {
 			NonStreamResponser(&bodyCopy, w, resp, &users[0])
 		}
 	} else {
-		bodyCopy, readErr := io.ReadAll(r.Body)
-		if readErr != nil {
-			fmt.Printf("error reading body: %s\n", readErr)
-		}
-		path := path.Clean(r.URL.Path)
-		req, err := http.NewRequest(strings.ToUpper(r.Method), "https://"+domain+path, bytes.NewBuffer(bodyCopy))
-		for k, v := range r.Header {
-
-			req.Header.Add(k, v[0])
-
-		}
-		req.Header.Add("Access-Control-Allow-Origin", "*")
-		client := &http.Client{Timeout: 50 * time.Second}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Printf("error making request: %s\n", err)
-		}
-		fmt.Println("different kind of request")
-		NormalStreamResponser(resp, w)
+		NormalResponse(w, r)
 	}
+}
+func NormalResponse(w http.ResponseWriter, r *http.Request) {
+	bodyCopy, readErr := io.ReadAll(r.Body)
+	if readErr != nil {
+		fmt.Printf("error reading body: %s\n", readErr)
+	}
+	path := path.Clean(r.URL.Path)
+	req, err := http.NewRequest(strings.ToUpper(r.Method), "https://"+domain+path, bytes.NewBuffer(bodyCopy))
+	for k, v := range r.Header {
+
+		req.Header.Add(k, v[0])
+
+	}
+	req.Header.Add("Access-Control-Allow-Origin", "*")
+	client := &http.Client{Timeout: 50 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("error making request: %s\n", err)
+	}
+	fmt.Println("different kind of request")
+	NormalStreamResponser(resp, w)
 }
